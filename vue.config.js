61c094283d222538f91d340aafb9d5e7826aaab7
WingCH <wingchan@vgt-hk.net>
2020-04-06 17:07:51 +0800

    Revert "add google analytics"
    This reverts commit 3137d2486128bd87d74997d69bad9d8c312e4d39.

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   configureWebpack: {
     devtool: "source-map"
-  },
-  plugins: [
-    [
-      '@vuepress/google-analytics',
-      {
-        'ga': 'UA-75630254-2' // UA-00000000-0
-      }
-    ]
-  ]
+  }
 };
