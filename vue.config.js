3137d2486128bd87d74997d69bad9d8c312e4d39
WingCH <wingchan@vgt-hk.net>
2020-04-06 16:54:46 +0800

    add google analytics

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   configureWebpack: {
     devtool: "source-map"
-  }
+  },
+  plugins: [
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-75630254-2' // UA-00000000-0
+      }
+    ]
+  ]
 };
